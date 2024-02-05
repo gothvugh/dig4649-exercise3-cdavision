f105e3da4e4a6822fb3813b36d0c4cfbf527a537
cole-davison <122580230+cole-davison@users.noreply.github.com>
2024-02-05 16:32:20 -0500

    Created quote app.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dig4649-exercise3-cdavision
 Repository assignment for DIG4639 - Exercise3
+
+Favorite Quotation: "It is what it is."
---
+++ quoteApp.js
@@ -0,0 +1,7 @@
+favoriteQuotations = ["It is what it is.", "Womp womp.", "Do you think Jesus Christ, the son of the carpenter, smelt the wood of the cross and thought of home?"];
+
+let randomIndex = Math.floor(Math.random()*favoriteQuotations.length);
+let randomQuote = favoriteQuotations[randomIndex];
+
+
+console.log(randomQuote);
